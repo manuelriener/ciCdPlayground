e2e3008941d4d0bbf7d11ffac43469fd42634e30
manuelriener <manuel.riener@tngtech.com>
2025-01-24 13:49:13 +0100

    Update information in main

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "M. Riener",
+    name: "Manuel Riener",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Continuous integration, continuous delivery"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "None yet",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["Basics about CI/CD, Jenkins"],
   },
 });
