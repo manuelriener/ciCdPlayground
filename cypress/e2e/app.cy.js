27e427172ce340bc4f087e376a40d5461c647b69
Manuel Riener <manuel.riener@tngtech.com>
2025-01-24 17:13:02 +0100

    Fix e2e test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -2,8 +2,8 @@
 //
 describe('The app', () => {
     it('tells that you are a butler', () => {
-        cy.visit('')
+        cy.visit('https://cicd-workshop-playground.s3.amazonaws.com/manuelriener/index.html')
         cy.get('h1')
-        .should('have.text', 'Hello, my name is James Butler!')
+        .should('have.text', 'Hello, my name is Manuel Riener!')
     })
   })
